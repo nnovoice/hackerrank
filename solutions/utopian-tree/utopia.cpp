94cf7ee1260d95e7035930ef730845c1d8e3d700
n.raghavendran <n.raghavendran@gmail.com>
2014-06-27 14:14:57 +0530

    Misunderstood the problem

--- solutions/utopian-tree/utopia.cpp
+++ solutions/utopian-tree/utopia.cpp
@@ -9,7 +9,13 @@
 
 int GetTreeHeight(int nSeasons)
 {
+	if (nSeasons == 0) return 1;
 
+	if (nSeasons % 2 == 0) {
+		return (nSeasons / 2) * 3 + 1;
+	}
+
+	return (nSeasons / 2) * 3 + 1 + 1;
 }
 
 int main()
