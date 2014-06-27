92bd43a3536ad929668ff8b84978de77fc803400
n.raghavendran <n.raghavendran@gmail.com>
2014-06-27 14:14:17 +0530

    Syncing first set of commits

--- solutions/utopian-tree/utopia.cpp
+++ solutions/utopian-tree/utopia.cpp
@@ -9,13 +9,7 @@
 
 int GetTreeHeight(int nSeasons)
 {
-	if (nSeasons == 0) return 1;
 
-	if (nSeasons % 2 == 0) {
-		return (nSeasons / 2) * 3 + 1;
-	}
-
-	return (nSeasons / 2) * 3 + 1 + 1;
 }
 
 int main()
