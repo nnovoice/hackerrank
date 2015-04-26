ae949b8fcb1b09e4a5a74ddf06cf0a9978a5290a
Raghavendran N <n.raghavendran@gmail.com>
2015-04-26 22:39:09 +0530

    bad logic so far. the even numbers get counted twice.

---
+++ solutions/project-euler/02-sum-of-even-fibo.py
@@ -0,0 +1,24 @@
+T = int(raw_input())
+print T
+
+sum = 0;
+
+for i in range(0,T):
+    N = int(raw_input())
+    print "N is ", N
+    sum = 0
+    a = 1
+    b = 2
+    while(True):
+        if (a > N or b > N):
+            break
+
+        a, b = b, a+b
+        if (b % 2 == 0):
+		    sum += b
+        else:
+		    if (a % 2 == 0):
+			    sum += a
+        print "a=",a, "b=",b, "sum=", sum
+    print sum
+    
