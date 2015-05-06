26009bf50e2b488a1ed0f9d09df65a266c568282
Raghavendran N <n.raghavendran@gmail.com>
2015-05-06 08:38:08 +0530

    AC with N^2 solution.

--- solutions/project-euler/08-largest-product.cpp
+++ solutions/project-euler/08-largest-product.cpp
@@ -3,28 +3,27 @@ using namespace std;
 
 int main()
 {
-    int T = 0,N = 0, K = 0;
+    int T = 0, n = 0, k = 0;
     unsigned long long best_prod = 0, cur_prod = 0;
     cin >> T;
     char digits[1001];
     char c;
     while (T--) {
-        cin >> N >> K;
-        cin >> c;
+        cin >> n >> k;
+        //cin >> c;
         cin >> digits;
-        cout << N << " " << K <<  " " << digits << endl;
-        cur_prod = digits[0] - '0';
+        //cout << "n= " << n << " k= " << k <<  " digits= " << digits << endl;
         best_prod = 0;
-        for (int i = 1; i < N; ++i) {
-            if ((i+1) % K == 0) {
-                cout << "debug: " << "i = " << i << " K = " << K << " i/K= " << i/K << endl;
-                if (cur_prod > best_prod) best_prod = cur_prod;
-                cur_prod /= digits[i/K * K - 1];
+        for (int i = 0; i <= n - k; ++i) {
+            cur_prod = 1;
+            for (int j = i; j < i + k; ++j) {
+                cur_prod *= (digits[j] - '0');
             }
-            cur_prod *= digits[i] - '0';
+            if (cur_prod > best_prod) 
+                best_prod = cur_prod;
+            //cout << "i= " << i << " best = " << best_prod << endl;
         }
         cout << best_prod << endl;
-
     }
     return 0;
 }
