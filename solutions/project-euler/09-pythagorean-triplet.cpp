bb7da397e8a04824e7e8061cc56a4856991e38b3
Raghavendran N <n.raghavendran@gmail.com>
2015-05-06 23:37:49 +0530

    Changed logic using mimino's ideas in the 24 hour coding challenge. iterate over c first, then over b and then determine a. super idea. just one test case failing. need to check that.

--- solutions/project-euler/09-pythagorean-triplet.cpp
+++ solutions/project-euler/09-pythagorean-triplet.cpp
@@ -3,38 +3,25 @@ using namespace std;
 
 int main()
 {
-    unsigned int T = 0, N = 0;
-    unsigned int best_a = 0, a, best_b = 0, b, best_c = 0, c;
-    unsigned int prod = 0, best_prod = 0;
+    int T = 0, N = 0;
+    int best_a = 0, a, best_b = 0, b, best_c = 0, c;
+    int prod = 0, best_prod = 0;
     cin >> T;
     while (T--) {
         cin >> N;
-        //cout << "debug: " << " N= " << N << endl;
-        best_prod = 0;
-        for (a = 2; a < N; ++a) {
-            for (b = a + 1; b < N; ++b) {
-                c = N - a - b;
-                if (a > c || b > c)
-                    break;
+        best_prod = -1;
+        for (int c = 1; c < N/2; ++c) {
+            for (int b = 1; b < c; ++b) {
+                a = N - b - c;
+                if (a < 0) continue;
                 if (a*a + b*b == c*c) {
                     prod = a*b*c;
-                    if (prod > best_prod) {
+                    if (prod > best_prod)
                         best_prod = prod;
-                        best_a = a;
-                        best_b = b;
-                        best_c = c;
-                    }
                 }
             }
         }
-        //a = best_a;
-        //b = best_b;
-        //c = best_c;
-        //cout << "a = " << a << " b= " << b << " c= " << c << " a+b+c=" << (a+b+c)<< " N= " << N << " a*b*c= " << a*b*c << endl; 
-        if (best_prod > 0)
-            cout << best_prod << endl;
-        else
-            cout << -1 << endl;
+        cout << best_prod << endl;
     }
     return 0;
 }
