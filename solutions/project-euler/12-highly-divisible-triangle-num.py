2d2d4dc888308b2d4b62fd0b353ab088b6183e56
Raghavendran N <n.raghavendran@gmail.com>
2015-05-11 09:08:56 +0530

    python code for 12th problem helps in pre-computation of the triangle number and its divisors. when compared with mimino's logic, this one seems too big and buggy too. need to simplify

---
+++ solutions/project-euler/12-highly-divisible-triangle-num.py
@@ -0,0 +1,78 @@
+MAX = 50001
+NUM_TRINUMS = 42000
+primes = []
+ps = [] #prime slots
+tfactors = [0] * 1026 
+
+def gen_primes(n):
+    ps = [True] * n
+    ps[0] = False
+    ps[1] = False
+    for i in range(2, n):
+        if (ps[i] == True):
+            primes.append(i)
+            j = i + i
+            while (j < MAX):
+                ps[j] = False
+                j += i
+    #print "num primes= ", len(primes)
+    #for i in range(len(primes)):
+    #    print i, primes[i]
+
+def get_prime_factors(n):
+    f = []
+    j = 0
+    #print "get_prime_factors: ", n, j, f, primes[j]
+    while (n > 0 and primes[j] <= n):
+        if (n % primes[j] == 0):
+            #print n, j, f, primes[j]
+            while (n > 0):
+                #print "ok", n, j, f, primes[j]
+                if (n % primes[j] == 0):
+                    f.append(primes[j])
+                    #print "appended ", primes[j]
+                    n /= primes[j]
+                else:
+                    break
+        j += 1
+
+    if (len(f) == 0):
+        f.append(n)
+        #print primes[j], n, "appended: ", n
+    return f
+
+trinums = []
+def gen_trinums(n):
+    trinums.append(1)
+    for i in range(1,n):
+        trinums.append(trinums[i-1] + i + 1)
+        #print trinums[i]
+
+gen_trinums(NUM_TRINUMS)
+gen_primes(MAX)
+max_factors = 0
+tfactors[0] = 0
+tfactors[1] = 1
+
+def print_tfactors():
+    for i in range(1, 1025):
+        print i, tfactors[i]
+
+for i in range(1,NUM_TRINUMS):
+    factors = get_prime_factors(trinums[i])
+    nfactors = 1
+    for f in set(factors):
+        a = factors.count(f)
+        nfactors *= (a + 1)
+
+    if (tfactors[nfactors] == 0):
+        tfactors[nfactors] = trinums[i]
+
+    #print trinums[i], factors, "nfactors= ", nfactors
+    if (nfactors > max_factors):
+        max_factors = nfactors
+
+print "max factors= ", max_factors
+print_tfactors()
+
+
