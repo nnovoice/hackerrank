f0e7dea9e4444a08217589fb6be4fe695c9b92bb
Raghavendran N <n.raghavendran@gmail.com>
2015-06-02 09:16:35 +0530

    WA. The logic is to first generate the pascal's triangle and move each column up so that there are no zeroes. That makes each value to be the same as the number of ways to reach each cell. Refer Wikipedia article on pascal's triangle

--- solutions/project-euler/15-lattice-paths.cpp
+++ solutions/project-euler/15-lattice-paths.cpp
@@ -3,34 +3,65 @@ using namespace std;
 
 typedef unsigned long long int ulli;
 
-const int MAX = 507;
+const int MAX = 1007;
 ulli tri[MAX][MAX];
 
 void gen_pascal_triangle()
 {
     tri[1][0] = 0;
     tri[1][1] = 1;
-    cout << "1: " << tri[1][1] << endl;
     for (int i = 2; i < MAX; ++i) {
-        cout << i << ": ";
         for (int j = 1; j < MAX; ++j) {
             tri[i][j] = tri[i-1][j] + tri[i-1][j-1];
             if (tri[i][j] == 0)
                 break;
-            cout << tri[i][j] << "\t";
+        }
+    }
+}
+
+void move_triangle_to_square()
+{
+    int i = 1, j = 1, k = 1;
+    
+    for (; i < MAX; ++i) {
+        k = 1;
+        for (j = 1; j < MAX; ++j)
+            if (tri[j][i] == 0)
+                continue;
+            else 
+                break;
+
+        for (; j < MAX; ++j, ++k) {
+            tri[k][i] = tri[j][i];                
+        }
+    }
+}
+
+void print_tri() {
+    for (int i = 1; i < MAX; ++i) {
+        for (int j = 1; j < MAX; ++j) {
+            cout << tri[i][j] << " ";
         }
         cout << endl;
     }
+
 }
 
 int main()
 {
     gen_pascal_triangle();
+    move_triangle_to_square();
+    //print_tri();
 
     int T = 0, N = 0, M = 0;
     cin >> T;
+    cout << "cases= " << T << endl;
     while (T--) {
         cin >> N >> M;
-        cout << tri[N*M+1][M+1] << endl;
+        if (N == 1 || M == 1)
+            cout << tri[N][M] << endl;
+        else
+            cout << tri[N+1][M+1] << endl;
     }
+    return 0;
 }
